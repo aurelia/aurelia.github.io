07cef7124113e8fb813c2ca8853e85dfc5c90fb4
Dwayne Charrington <dwaynecharrington@gmail.com>
2024-12-15 08:29:29 +1000

    feat(site): move back to separate view/viewmodel, show html
    We need to highlight the simplicity of Aurelia

--- aurelia-apps/src/examples/simple-counter.ts
+++ aurelia-apps/src/examples/simple-counter.ts
@@ -1,15 +1,3 @@
-import { customElement } from 'aurelia';
-
-@customElement({
-  name: 'simple-counter',
-  template: `
-    <p style="margin-top:0;">Current count: \${count}</p>
-    <button click.trigger="increment()">Increment</button>
-  `,
-  shadowOptions: {
-    mode: 'open',
-  },
-})
 export class SimpleCounter {
   count = 0;
 
