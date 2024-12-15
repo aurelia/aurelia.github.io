764dcd2a1ae5fd592ab3af2a1acc94999c8adb71
Dwayne Charrington <dwaynecharrington@gmail.com>
2024-12-16 09:04:28 +1000

    feat(site): use postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    '@tailwindcss/postcss': {
+      config: './tailwind.config.js',
+    },
+    'autoprefixer': {},
+  }
 };
